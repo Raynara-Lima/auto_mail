131bdb9719bda98b03af22e380d0c500d42ab55c
unknown <raynaralima17@gmail,com>
2019-09-08 22:12:29 -0300

    First commit

---
+++ server/server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get('/api/mensagem', (req, res) => {
+  res.send({ express: 'Hello From Express' });
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
+// Imports the Google Cloud client library
+const {PubSub} = require('@google-cloud/pubsub');
+
+// Creates a client
+const pubsub = new PubSub();
+
+/**
+ * TODO(developer): Uncomment the following lines to run the sample.
+ */
+const subscriptionName = 'Web_App';
+ const timeout = 60;
+
+// References an existing subscription
+const subscription = pubsub.subscription(subscriptionName);
+
+// Create an event handler to handle messages
+let messageCount = 0;
+const messageHandler = message => {
+  console.log(`Received message ${message.id}:`);
+  console.log(`\tData: ${message.data}`);
+  console.log(`\tAttributes: ${message.attributes}`);
+  messageCount += 1;
+
+  // "Ack" (acknowledge receipt of) the message
+  message.ack();
+};
+
+// Listen for new messages until timeout is hit
+subscription.on(`message`, messageHandler);
+
+setTimeout(() => {
+  subscription.removeListener('message', messageHandler);
+  console.log(`${messageCount} message(s) received.`);
+}, timeout * 1000);
