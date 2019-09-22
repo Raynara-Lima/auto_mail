dbcbc38520d56c3af7b31b52c20500d8daf27ab2
Raynara <raynaralima17@gmail.com>
2019-09-21 21:47:08 -0300

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,45 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ImageMapper from 'react-image-mapper';
+import axios from 'axios';
+import {FormControl, Button,Col, Row} from 'react-bootstrap';
 
+import './App.css';
+const URL = "./mapa2.png"
+const MAP = {
+  name: "my-map",
+  areas: [
+    // { name: "1", shape: "poly", coords: [25,33,27,300,128,240,128,94], preFillColor: "green", fillColor: "blue"  },
+    { name: "2", shape: "poly", coords: [219,118,220,210,283,210,284,119], preFillColor: "pink"  },
+    // { name: "3", shape: "poly", coords: [381,241,383,94,462,53,457,282], fillColor: "yellow"  },
+    // { name: "4", shape: "poly", coords: [245,285,290,285,274,239,249,238], preFillColor: "red"  },
+    // { name: "5", shape: "circle", coords: [170, 100, 25 ] },
+  ]
+}
 class App extends Component {
   state = {
-	response: ''
+	response: '',
+	hoveredArea: '',
+	msg: '',
+	moveMsg: '',
+	data: '',
+	mensagem: '',
+	mensagemRecebida: '',
+	map:  {name: "my-map",
+	areas: [
+	  // { name: "1", shape: "poly", coords: [25,33,27,300,128,240,128,94], preFillColor: "green", fillColor: "blue"  },
+	  { name: "2", shape: "poly", coords: [130,220,130,240,150,240,150,220], preFillColor: "pink"  },
+	  { name: "2", shape: "poly", coords: [130,220,130,240,150,240,150,220], preFillColor: "black"  },
+
+	  // { name: "3", shape: "poly", coords: [381,241,383,94,462,53,457,282], fillColor: "yellow"  },
+	  // { name: "4", shape: "poly", coords: [245,285,290,285,274,239,249,238], preFillColor: "red"  },
+	  // { name: "5", shape: "circle", coords: [170, 100, 25 ] },
+	]}
   };
 
   componentDidMount() {
 	this.callApi()
   	.then(res => this.setState({ response: res.express }))
-  	.catch(err => console.log(err));
+	  .catch(err => console.log(err));
   }
 
   callApi = async () => {
@@ -20,18 +49,126 @@ class App extends Component {
 
 	return body;
   };
+  enterArea(area) {
+    this.setState({ hoveredArea: area });
+}
+load() {
+	this.setState({ msg: "Interact with image !" });
+}
 
+leaveArea(area) {
+    this.setState({ hoveredArea: null });
+}
+
+getTipPosition(area) {
+    return { top: `${area.center[1]}px`, left: `${area.center[0]}px` };
+}
+moveOnImage(evt) {
+	const coords = { x: evt.nativeEvent.layerX, y: evt.nativeEvent.layerY };
+	this.setState({
+		moveMsg: `You moved on the image at coords ${JSON.stringify(coords)} !`
+	});
+}
+moveOnArea(area, evt) {
+	const coords = { x: evt.nativeEvent.layerX, y: evt.nativeEvent.layerY };
+	this.setState({
+		moveMsg: `You moved on ${area.shape} ${
+			area.name
+		} at coords ${JSON.stringify(coords)} !`
+	});
+}
+clicked(area) {
+	this.setState({
+		msg: `You clicked on ${area.shape} at coords ${JSON.stringify(
+			area.coords
+		)} !`
+	});
+}
+clickedOutside(evt) {
+	const coords = { x: evt.nativeEvent.layerX, y: evt.nativeEvent.layerY };
+	this.setState({
+		msg: `You clicked on the image at coords ${JSON.stringify(coords)} !`
+	});
+}
+getMensagem = () => {
+    axios.get('http://localhost:8080/api/getData', {mode:'no-cors'})
+	.then(response => {
+		this.setState({mensagemRecebida: response.data.data});
+		console.log(response.data)
+		}	).catch(err => console.log(err));
+
+  };
+  setMensagem = () => {
+    axios('http://localhost:8080/api/setData', {
+		mode:'no-cors',
+		method: 'POST',
+		// headers:{'Content-Type' : 'application/json', 'Access-Control-Allow-Origin': '*'},
+		  params: JSON.stringify({msg: this.state.mensagem})
+		})
+	.then(response => this.setState({mensagemRecebida: ''}))
+			.catch(err => console.log(err));
+
+  };
   render() {
 	return (
-  	<div className="App">
-    	<header className="App-header">
-      	<img src={logo} className="App-logo" alt="logo" />
-      	<h1 className="App-title">Welcome to React</h1>
-    	</header>
-    	<p className="App-intro">{this.state.response}</p>
-  	</div>
+  	<div className="container">
+		  <Row style={{paddingTop: '15px'}}>
+			  <Col md={12}>
+			  <Col md={6}>
+		 	 <img src='./logoAutoMail.jpg' width='30%' />
+			</Col>
+			{/* <Col md={6}>Estação de Controle</Col> */}
+			</Col>
+		  </Row>
+		  <Row style={{paddingTop: '15px'}}>
+		  <Col md={9}>
+
+			  <Col md={6}>
+			  <FormControl type="text" value={this.state.mensagem} onChange={(event) => this.setState({mensagem: event.target.value})}/>
+			  </Col>
+			  <Col md={6}>
+				  <Button onClick={this.setMensagem}>Enviar</Button>
+				  </Col>
+				  </Col>
+		  </Row>
+		  <Row style={{paddingTop: '15px'}}>
+		  <Col md={6}>
+			<Col md={3}>
+		  		<Button onClick={this.getMensagem}>Ver mensagem</Button>
+			</Col>
+			<Col md={8}>
+				  <FormControl type="text" value={this.state.mensagemRecebida }  disabled/>
+		  </Col>
+	</Col>
+		  </Row>
+		  <Row style={{paddingTop: '70px'}}>
+			  {/* <img src='./mapa.jpg'></img> */}
+     <ImageMapper src={URL} map={this.state.map} width={1000}
+    	onLoad={() => this.load()}
+    	onClick={area => this.clicked(area)}
+    	onMouseEnter={area => this.enterArea(area)}
+    	onMouseLeave={area => this.leaveArea(area)}
+    	onMouseMove={(area, _, evt) => this.moveOnArea(area, evt)}
+    	onImageClick={evt => this.clickedOutside(evt)}
+    	onImageMouseMove={evt => this.moveOnImage(evt)}
+    />
+   {	this.state.hoveredArea &&
+    	<span className="tooltip"
+    	    style={{ ...this.getTipPosition(this.state.hoveredArea)}}>
+    		{ this.state.hoveredArea && this.state.hoveredArea.name}
+    	</span>
+	}
+	<pre className="message">
+						{this.state.msg ? this.state.msg : null}
+					</pre>
+					<pre>{this.state.moveMsg ? this.state.moveMsg : null}</pre>
+  	
+	  </Row>
+	  </div>
+	  
 	);
   }
 }
+
 
 export default App;
--- server/server.js
+++ server/server.js
@@ -1,23 +1,36 @@
 const express = require('express');
+const router = express.Router();
+var cors = require('cors')
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
-app.get('/api/mensagem', (req, res) => {
-  res.send({ express: 'Hello From Express' });
-});
+const bodyParser = require("body-parser");
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
-// Imports the Google Cloud client library
+/** bodyParser.urlencoded(options)
+ * Parses the text as URL encoded data (which is how browsers tend to send form data from regular forms set to POST)
+ * and exposes the resulting object (containing the keys and values) on req.body
+ */
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+/**bodyParser.json(options)
+ * Parses the text as JSON and exposes the resulting object on req.body.
+ */
+app.use(bodyParser.json());
+
 const {PubSub} = require('@google-cloud/pubsub');
 
 // Creates a client
 const pubsub = new PubSub();
 
-/**
- * TODO(developer): Uncomment the following lines to run the sample.
- */
-const subscriptionName = 'Web_App';
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+
+router.get('/getData' , (req, res) => {
+  const subscriptionName = 'teste';
  const timeout = 60;
 
 // References an existing subscription
@@ -25,20 +38,61 @@ const subscription = pubsub.subscription(subscriptionName);
 
 // Create an event handler to handle messages
 let messageCount = 0;
+let msg = ''
 const messageHandler = message => {
   console.log(`Received message ${message.id}:`);
   console.log(`\tData: ${message.data}`);
   console.log(`\tAttributes: ${message.attributes}`);
   messageCount += 1;
+console.log( JSON.parse(message.data).foo)
+msg =JSON.parse(message.data).foo
 
+console.log( `${message.data}`)
   // "Ack" (acknowledge receipt of) the message
+
   message.ack();
+  let data = msg
+    return res.json({ success: true, data: data });
 };
 
-// Listen for new messages until timeout is hit
+//Listen for new messages until timeout is hit
 subscription.on(`message`, messageHandler);
 
 setTimeout(() => {
   subscription.removeListener('message', messageHandler);
   console.log(`${messageCount} message(s) received.`);
 }, timeout * 1000);
+
+  res.setHeader('Access-Control-Allow-Origin', '*');  
+
+
+});
+
+router.post('/setData' , (req, res) => {
+const topicName = 'teste';
+console.log(req.query[0])
+
+let teste = JSON.parse(req.query[0])
+
+ const data = JSON.stringify({ foo: teste.msg });
+
+// Publishes the message as a string, e.g. "Hello, world!" or JSON.stringify(someObject)
+const dataBuffer = Buffer.from(data);
+
+
+
+publishMessage(topicName, dataBuffer)
+
+  res.setHeader('Access-Control-Allow-Origin', '*');  
+    return res.json({ success: true});
+})
+async function publishMessage(topicName, dataBuffer){
+  const messageId = await pubsub.topic(topicName).publish(dataBuffer);
+  console.log(`Message ${messageId} published.`);
+  }
+app.use('/api', router);
+app.use(cors())
+
+
+
+
